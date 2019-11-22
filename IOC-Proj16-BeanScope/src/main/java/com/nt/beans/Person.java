002bab7c2c3386c2abf78b8a892469ff2e920fe9
rajat <rajatrsmt@gmail.com>
2019-11-23 00:09:37 +0530

    Spring project

---
+++ IOC-Proj16-BeanScope/src/main/java/com/nt/beans/Person.java
@@ -0,0 +1,16 @@
+package com.nt.beans;
+
+public class Person {
+	public Person() {
+		System.out.println("Person::0-param constructor");
+	}
+public  void eating() {
+	System.out.println("Person:eathing");
+	}
+public void dancing() {
+	System.out.println("Person:dancing");
+}
+
+
+
+}
